ad48b05c2a534f99e667a94ef4e0348cb33f23f7
b-sharman <nonoesimposible72@gmail.com>
2023-12-29 21:34:54 -0700

    recursive fib implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn recursion(n: i32) -> i32 {
+    match n {
+        0 | 1 => 1,
+        _ => recursion(n-1) + recursion(n-2)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("recursion:");
+    for i in 0..10 {
+        println!("{}: {}", i, recursion(i));
+    }
 }
