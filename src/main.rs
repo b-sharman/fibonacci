c454eaa61b3b810bfdbe4aca7dc325cf92be4ed8
b-sharman <nonoesimposible72@gmail.com>
2023-12-31 12:28:21 -0700

    write a couple unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use std::collections::HashMap;
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_recursion() {
+        assert_eq!(1, recursion(0));
+        assert_eq!(1, recursion(1));
+        assert_eq!(8, recursion(5));
+    }
+
+    #[test]
+    fn test_dp() {
+        let mut dp = DP::new();
+        assert_eq!(1, dp.nth_fib(0));
+        assert_eq!(1, dp.nth_fib(1));
+        assert_eq!(8, dp.nth_fib(5));
+    }
+}
+
 fn recursion(n: u32) -> u32 {
     match n {
         0 | 1 => 1,
