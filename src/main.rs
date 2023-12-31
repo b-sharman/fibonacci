085adc2e523845f7d58d1044da0d787c00de89e7
b-sharman <nonoesimposible72@gmail.com>
2023-12-30 18:12:52 -0700

    add dynamic programming implementation
    really shows the difference between O(k^n) and O(n)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
-fn recursion(n: i32) -> i32 {
+use std::collections::HashMap;
+
+fn recursion(n: u32) -> u32 {
     match n {
         0 | 1 => 1,
         _ => recursion(n-1) + recursion(n-2)
     }
 }
 
+fn dp(cache: &mut HashMap<u32, u32>, n: u32) -> u32 {
+    match cache.get(&n) {
+        Some(x) => *x,
+        None => {
+            let retval = dp(cache, n-1) + dp(cache, n-2);
+            cache.insert(n, retval);
+            retval
+        }
+    }
+}
+
 fn main() {
     println!("recursion:");
-    for i in 0..10 {
+    for i in 0..40 {
         println!("{}: {}", i, recursion(i));
+    }
+
+    let mut cache: HashMap<u32, u32> = HashMap::new();
+    cache.insert(0, 1);
+    cache.insert(1, 1);
+    println!("\ndp:");
+    for i in 0..40 {
+        println!("{}: {}", i, dp(&mut cache, i));
     }
 }
